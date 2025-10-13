2a0e9f80eebd87c074a855d343892a67391a22f3
Ricardojr <122828004+RickzinJR@users.noreply.github.com>
2025-10-13 12:37:31 -0300

    dashboard

--- src/Entrega 1/Frontend/dashboard/script.js
+++ src/Entrega 1/Frontend/dashboard/script.js
@@ -1,4 +1,7 @@
+<<<<<<< HEAD
+=======
 
+>>>>>>> 1be6aca3344aa7e93b3a85cd8537a9eaf872dd32
 const hamburger = document.getElementById("hamburger");
 const dropdown = document.getElementById("dropdown");
 const overlay = document.getElementById("overlay");
@@ -15,8 +18,13 @@ overlay.addEventListener("click", () => {
   hamburger.classList.remove("active");
 });
 
+<<<<<<< HEAD
+dropdown.querySelectorAll("a").forEach((link) => {
+  link.addEventListener("click", () => {
+=======
 dropdown.querySelectorAll('a').forEach(link => {
   link.addEventListener('click', () => {
+>>>>>>> 1be6aca3344aa7e93b3a85cd8537a9eaf872dd32
     dropdown.classList.remove("show");
     overlay.classList.remove("show");
     hamburger.classList.remove("active");
