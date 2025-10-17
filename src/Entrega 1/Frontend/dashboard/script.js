dc59af0ce0aa23aebc095357f7d71a70b29c88fa
Ricardojr <122828004+RickzinJR@users.noreply.github.com>
2025-10-17 08:50:51 -0300

    Dashboard

--- src/Entrega 1/Frontend/dashboard/script.js
+++ src/Entrega 1/Frontend/dashboard/script.js
@@ -1,32 +1,16 @@
-<<<<<<< HEAD
-=======
 
->>>>>>> 1be6aca3344aa7e93b3a85cd8537a9eaf872dd32
-const hamburger = document.getElementById("hamburger");
-const dropdown = document.getElementById("dropdown");
-const overlay = document.getElementById("overlay");
+const hamburger = document.getElementById('hamburger');
+const dropdown = document.getElementById('dropdown');
+const overlay = document.getElementById('overlay');
 
-hamburger.addEventListener("click", () => {
-  dropdown.classList.toggle("show");
-  overlay.classList.toggle("show");
-  hamburger.classList.toggle("active");
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  dropdown.classList.toggle('show');
+  overlay.classList.toggle('show');
 });
 
-overlay.addEventListener("click", () => {
-  dropdown.classList.remove("show");
-  overlay.classList.remove("show");
-  hamburger.classList.remove("active");
-});
-
-<<<<<<< HEAD
-dropdown.querySelectorAll("a").forEach((link) => {
-  link.addEventListener("click", () => {
-=======
-dropdown.querySelectorAll('a').forEach(link => {
-  link.addEventListener('click', () => {
->>>>>>> 1be6aca3344aa7e93b3a85cd8537a9eaf872dd32
-    dropdown.classList.remove("show");
-    overlay.classList.remove("show");
-    hamburger.classList.remove("active");
-  });
+overlay.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  dropdown.classList.remove('show');
+  overlay.classList.remove('show');
 });
